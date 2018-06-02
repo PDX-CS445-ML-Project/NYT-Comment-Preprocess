0a45bd0a1f93ef40b65b9f1ca64597f3c811ea35
magicbonsai <alexzeyanzhu@gmail.com>
2018-06-02 15:03:07 -0700

    Preliminary filtering of unwanted unicode

--- CSVParser.py
+++ CSVParser.py
@@ -16,8 +16,9 @@ def pDict(comments_location, articles_location):
         if row['commentBody'] == "":  # if empty, skip it
             continue
         else:
+            string = pfilter(row['commentBody'])
             #comments[row['articleID']] = row['commentBody']
-            comments[row['articleID']].append(row['commentBody'])
+            comments[row['articleID']].append(string)
     reader = csv.DictReader(open(articles_location, encoding='utf-8'))
     for row in reader:
         if row['articleID'] not in comments.keys():
@@ -25,6 +26,20 @@ def pDict(comments_location, articles_location):
         else:
             categories[row['articleID']] = row['keywords'] #jsons.load() wasn't needed here?
 
+def pfilter(string):
+    # get rid of common unicode characters that aren't letters
+    string = string.lower().replace("<br/><br/>", " ")
+    string = string.replace("<br/>", " ")
+    string = string.replace('\"', " ")
+    string = string.replace("\u201c", " ")
+    string = string.replace("\u201d", " ")
+    string = string.replace("\u2014", " ")
+    string = string.replace("\u2015", " ")
+    string = string.replace("\u2016", " ")
+    string = string.replace("\u2017", " ")
+    string = string.replace("\u2018", " ")
+    string = string.replace("\u2019", " ")
+    return string
 
 if __name__ == '__main__':
 
